be5a00ca494c06d20888b6a7c7010f48472121a3
tharindra galahena <tcg.galahena@gmail.com>
2012-06-27 21:21:52 +0530

    c1_start

---
+++ src/demo.c
@@ -0,0 +1,229 @@
+#include <gtk/gtk.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <strings.h>
+#include <glib.h>
+
+#include "graph.h"
+
+graph *g;
+
+GtkWidget *window_main;
+GtkWidget *box_main;
+GtkWidget* image_headder;
+GtkWidget* text_x_max;
+GtkWidget* text_x_min;
+GtkWidget* text_y_max;
+GtkWidget* text_y_min;
+
+
+double func1(double x){	
+	return (x - 1)*(x - 2)*(x - 3)*(x - 4);
+}
+double func2(double x){	
+	return x*x*x + 3*x*x + 4;
+}
+double func3(double x){
+	return sin(x*3.14/180.0);
+}
+double func4(double x){
+	return log(x);
+}
+double func5(double x){
+	return cos(x*3.14/180.0);
+}
+double func6(double x){
+	return tan(x*3.14/180.0);
+}
+void re_draw(double (*f)(double),
+		   double range_x_min,
+		   double range_x_max,
+		   double range_y_min,
+		   double range_y_max){
+
+	clear_grapheft(g);
+	
+	double iter = (range_y_max - range_y_min)/20;
+	draw_graphid_y(g, range_y_min, range_y_max, iter, (char)0xAA);
+
+	iter = (range_x_max - range_x_min)/20;
+	draw_graphid_x(g, range_x_min, range_x_max,  iter, (char)0xAA);
+	create_y(g, range_y_min, range_y_max, 0, (char)0x00);
+
+	create_x(g, range_x_min, range_x_max, 0, (char)0x00);
+	draw_grapheft(g, (*f), 
+			 range_x_min, range_x_max, range_y_min, range_y_max);
+
+	write_bitmap(g, "a.bmp");
+}
+void b1(){
+
+	int x_max = atoi(gtk_entry_get_text(GTK_ENTRY(text_x_max)));
+	int x_min = atoi(gtk_entry_get_text(GTK_ENTRY(text_x_min)));
+	int y_max = atoi(gtk_entry_get_text(GTK_ENTRY(text_y_max)));
+	int y_min = atoi(gtk_entry_get_text(GTK_ENTRY(text_y_min)));
+
+	re_draw(func1, x_min, x_max, y_min, y_max);
+	gtk_image_set_from_file(GTK_IMAGE(image_headder), "a.bmp");
+}
+void b2(){
+	int x_max = atoi(gtk_entry_get_text(GTK_ENTRY(text_x_max)));
+	int x_min = atoi(gtk_entry_get_text(GTK_ENTRY(text_x_min)));
+	int y_max = atoi(gtk_entry_get_text(GTK_ENTRY(text_y_max)));
+	int y_min = atoi(gtk_entry_get_text(GTK_ENTRY(text_y_min)));
+
+	re_draw(func2, x_min, x_max, y_min, y_max);
+	
+	gtk_image_set_from_file(GTK_IMAGE(image_headder), "a.bmp");
+	gtk_widget_show(image_headder);
+}
+void b3(){
+	int x_max = atoi(gtk_entry_get_text(GTK_ENTRY(text_x_max)));
+	int x_min = atoi(gtk_entry_get_text(GTK_ENTRY(text_x_min)));
+	int y_max = atoi(gtk_entry_get_text(GTK_ENTRY(text_y_max)));
+	int y_min = atoi(gtk_entry_get_text(GTK_ENTRY(text_y_min)));
+	re_draw(func3, x_min, x_max, y_min, y_max);
+	
+	gtk_image_set_from_file(GTK_IMAGE(image_headder), "a.bmp");
+	gtk_widget_show(image_headder);
+}
+void b4(){
+	int x_max = atoi(gtk_entry_get_text(GTK_ENTRY(text_x_max)));
+	int x_min = atoi(gtk_entry_get_text(GTK_ENTRY(text_x_min)));
+	int y_max = atoi(gtk_entry_get_text(GTK_ENTRY(text_y_max)));
+	int y_min = atoi(gtk_entry_get_text(GTK_ENTRY(text_y_min)));
+	re_draw(func4, x_min, x_max, y_min, y_max);
+	
+	gtk_image_set_from_file(GTK_IMAGE(image_headder), "a.bmp");
+	gtk_widget_show(image_headder);
+}
+void b5(){
+	int x_max = atoi(gtk_entry_get_text(GTK_ENTRY(text_x_max)));
+	int x_min = atoi(gtk_entry_get_text(GTK_ENTRY(text_x_min)));
+	int y_max = atoi(gtk_entry_get_text(GTK_ENTRY(text_y_max)));
+	int y_min = atoi(gtk_entry_get_text(GTK_ENTRY(text_y_min)));
+	re_draw(func5, x_min, x_max, y_min, y_max);
+	
+	gtk_image_set_from_file(GTK_IMAGE(image_headder), "a.bmp");
+	gtk_widget_show(image_headder);
+}
+void b6(){
+	int x_max = atoi(gtk_entry_get_text(GTK_ENTRY(text_x_max)));
+	int x_min = atoi(gtk_entry_get_text(GTK_ENTRY(text_x_min)));
+	int y_max = atoi(gtk_entry_get_text(GTK_ENTRY(text_y_max)));
+	int y_min = atoi(gtk_entry_get_text(GTK_ENTRY(text_y_min)));
+
+	re_draw(func6, x_min, x_max, y_min, y_max);
+	
+	gtk_image_set_from_file(GTK_IMAGE(image_headder), "a.bmp");
+	gtk_widget_show(image_headder);
+}
+int main(int argc, char **argv){
+
+	g = init(1000, 500);
+	clear_grapheft(g);
+	write_bitmap(g, "a.bmp");
+
+	GtkWidget *box_buttons;
+	GtkWidget *box_text;
+
+	GtkWidget* label_x_min;
+	GtkWidget* label_x_max;
+	GtkWidget* label_y_min;
+	GtkWidget* label_y_max;
+
+	GtkWidget* button_new;
+	GtkWidget* button_rem;
+	GtkWidget* button_sin;
+	GtkWidget* button_tan;
+	GtkWidget* button_log;
+	GtkWidget* button_cos;
+
+	gtk_init(&argc, &argv);
+	window_main = gtk_window_new(GTK_WINDOW_TOPLEVEL);
+	
+	box_main = gtk_vbox_new (FALSE, 0);
+	box_buttons = gtk_hbox_new (FALSE, 0);
+	box_text = gtk_hbox_new (FALSE, 0);	
+
+	image_headder = gtk_image_new_from_file ("a.bmp");
+	gtk_box_pack_start(GTK_BOX (box_main), image_headder, FALSE, TRUE, 5);
+
+	text_x_max = gtk_entry_new ();
+	text_x_min = gtk_entry_new ();
+	text_y_max = gtk_entry_new ();
+	text_y_min = gtk_entry_new ();
+
+	label_x_min = gtk_label_new("min x");
+	label_x_max = gtk_label_new("max x");
+	label_y_min = gtk_label_new("min y");
+	label_y_max = gtk_label_new("max y");
+
+	gtk_box_pack_start (GTK_BOX (box_text), label_x_min, TRUE, FALSE, 5);
+	gtk_box_pack_start (GTK_BOX (box_text), text_x_min, TRUE, FALSE, 5);
+	gtk_box_pack_start (GTK_BOX (box_text), label_x_max, TRUE, FALSE, 5);
+	gtk_box_pack_start (GTK_BOX (box_text), text_x_max, TRUE, FALSE, 5);
+	gtk_box_pack_start (GTK_BOX (box_text), label_y_min, TRUE, FALSE, 5);
+	gtk_box_pack_start (GTK_BOX (box_text), text_y_min, TRUE, FALSE, 5);
+	gtk_box_pack_start (GTK_BOX (box_text), label_y_max, TRUE, FALSE, 5);
+	gtk_box_pack_start (GTK_BOX (box_text), text_y_max, TRUE, FALSE, 5);
+	gtk_box_pack_start(GTK_BOX (box_main), box_text, FALSE, TRUE, 5);
+
+
+	button_new = gtk_button_new_with_label ("(x - 1)(x - 2)(x - 3)(x - 4)");
+	button_rem = gtk_button_new_with_label ("x*x*x + 3*x*x + 4");
+	button_sin = gtk_button_new_with_label ("sin");
+	button_tan = gtk_button_new_with_label ("tan");
+	button_log = gtk_button_new_with_label ("log");
+	button_cos = gtk_button_new_with_label ("cos");
+	
+	gtk_signal_connect (GTK_OBJECT (button_new), "clicked",
+					GTK_SIGNAL_FUNC (b1), NULL);
+	gtk_box_pack_start (GTK_BOX (box_buttons), button_new, TRUE, FALSE, 5);
+	gtk_widget_show (button_new);
+	
+	gtk_signal_connect (GTK_OBJECT (button_rem), "clicked",
+					GTK_SIGNAL_FUNC (b2), NULL);
+	gtk_box_pack_start (GTK_BOX (box_buttons), button_rem, TRUE, FALSE, 5);
+	gtk_widget_show (button_rem);
+	
+	gtk_signal_connect (GTK_OBJECT (button_sin), "clicked",
+					GTK_SIGNAL_FUNC (b3), NULL);
+	gtk_box_pack_start (GTK_BOX (box_buttons), button_sin, TRUE, FALSE, 5);
+	gtk_widget_show (button_sin);
+	
+	gtk_signal_connect (GTK_OBJECT (button_log), "clicked",
+					GTK_SIGNAL_FUNC (b4), NULL);
+	gtk_box_pack_start (GTK_BOX (box_buttons), button_log, TRUE, FALSE, 5);
+	gtk_widget_show (button_log);
+	
+	gtk_signal_connect (GTK_OBJECT (button_cos), "clicked",
+					GTK_SIGNAL_FUNC (b5), NULL);
+	gtk_box_pack_start (GTK_BOX (box_buttons), button_cos, TRUE, FALSE, 5);
+	gtk_widget_show (button_cos);
+	
+	
+	gtk_signal_connect (GTK_OBJECT (button_tan), "clicked",
+					GTK_SIGNAL_FUNC (b6), NULL);
+	gtk_box_pack_start (GTK_BOX (box_buttons), button_tan, TRUE, FALSE, 5);
+	gtk_widget_show (button_tan);
+	
+	gtk_box_pack_start(GTK_BOX (box_main), box_buttons, FALSE, TRUE, 5);
+	gtk_container_add(GTK_CONTAINER (window_main), box_main);
+	gtk_widget_show(image_headder);
+	gtk_widget_show(text_x_max);
+	gtk_widget_show(text_x_min);
+	gtk_widget_show(text_y_max);
+	gtk_widget_show(text_y_min);
+	gtk_widget_show(label_x_max);
+	gtk_widget_show(label_x_min);
+	gtk_widget_show(label_y_max);
+	gtk_widget_show(label_y_min);
+	gtk_widget_show(box_text);
+	gtk_widget_show(image_headder);
+	gtk_widget_show(box_main);
+	gtk_widget_show(box_buttons);
+	gtk_widget_show(window_main);
+	gtk_main();
+	return 0;
+}
---
+++ src/graph.c
@@ -0,0 +1,247 @@
+#include <math.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+#define MAX_Y(a, b) ((a) > (b) ? (a) : (b))
+#define MIN_Y(a, b) ((a) < (b) ? (a) : (b))
+
+double get_scale(int a, double max, double min){
+	return (double)a / (max - min);
+}
+
+struct bmp_header * fill_header(int pixel_width, int pixel_height){
+
+    struct bmp_header *header = (struct bmp_header *)malloc(sizeof(struct bmp_header));
+    header -> BM = 0x4d42;
+    header -> size_of_file = sizeof(struct bmp_header) + 3 * pixel_width * pixel_height * 4;
+    header -> reserve = 0000;
+    header -> offset_of_pixle_data = 54;
+    header -> size_of_header = 40;
+    header -> width = pixel_width * 2;
+    header -> hight = pixel_height * 2;
+    header -> num_of_colour_plane = 1;
+    header -> num_of_bit_per_pix = 24;
+    header -> compression = 0;
+    header -> size_of_pix_data = 3 * pixel_width * pixel_height * 4;
+    header -> h_resolution = 2835;
+    header -> v_resolution = 2835;
+    header -> num_of_colour_in_palette = 0;
+    header -> important_colours = 0;
+    return header;
+}
+int create_y(graph *g, double range_y_min, double range_y_max, double y, char color){ 
+	if(range_y_max < y || range_y_min > y) return 0;
+	if(range_y_max - range_y_min <= 0) return 0;
+	char **pix = g -> pix;
+	double scale = get_scale(g -> pixel_height, range_y_max, range_y_min);
+	int Y = (int)(y*scale - range_y_min*scale);
+	int i;
+	for(i = 0; i < g -> pixel_width; i++){
+		pix[Y][i]	= color;
+	}
+	return 1;
+}
+int create_x(graph *g, double range_x_min, double range_x_max, double x, char color){ 
+	if(range_x_max < x || range_x_min > x) return 0;
+	if(range_x_max - range_x_min <= 0) return 0;
+	char **pix = g -> pix;
+	double scale = get_scale(g -> pixel_width, range_x_max, range_x_min);
+	int X = (int)(x*scale - range_x_min*scale);
+	int i;
+	for(i = 0; i < g -> pixel_height; i++){
+		pix[i][X]	= color;
+	}
+	return 1;
+}
+void write_bitmap(graph *g, char *name){
+	char **pix = g -> pix;
+
+	FILE *fp = fopen(name, "w");
+	fwrite ((char*)(g -> h), sizeof(struct bmp_header), 1, fp);
+	fwrite (&pix[0][0], 1, 1, fp);
+	int i, j, k;
+	for(i = 0; i < g -> pixel_height; i++){
+		for(j = 0; j < g -> pixel_width; j++){
+			for(k = 0; k < 6; k++){
+				fwrite (&pix[i][j], 1, 1, fp);			
+			}
+		}
+		for(j = 0; j < g -> pixel_width; j++){
+			for(k = 0; k < 6; k++){
+				fwrite (&pix[i][j], 1, 1, fp);			
+			}
+			
+		}	
+	}
+	fclose(fp);
+}
+void draw_graphid_y(graph *g, double range_y_min, double range_y_max, double iter, char color){
+	if(range_y_max - range_y_min <= 0) return;
+	char **pix = g -> pix;
+	double i = 0;
+	while(i < range_y_max){
+		create_y(g, range_y_min, range_y_max, i, color);
+		i += iter;
+	}
+	i = 0;
+	while(i > range_y_min){
+		create_y(g, range_y_min, range_y_max, i, color);
+		i -= iter;
+	}
+}
+void draw_graphid_x(graph *g, double range_x_min, double range_x_max, double iter, char color){
+	if(range_x_max - range_x_min <= 0) return;
+	char **pix = g -> pix;
+	double i = 0;
+	while(i < range_x_max){
+		create_x(g, range_x_min, range_x_max, i, color);
+		i += iter;
+	}
+	i = 0;
+	while(i > range_x_min){
+		create_x(g, range_x_min, range_x_max, i, color);
+		i -= iter;
+	}
+}
+double set_func(double (*f)(double), double x ) {
+    return (*f)(x);
+}
+void clear_grapheft(graph *g){
+	char **pix = g -> pix;
+	int i, j;
+	for(i = 0; i < g -> pixel_height; i++){
+		for(j = 0; j < g -> pixel_width; j++){
+			pix[i][j] = (char)0xFF;
+		}	
+	}
+}
+void draw_grapheft(graph *g,
+			 double (*f)(double),
+			 double range_x_min,
+			 double range_x_max,
+			 double range_y_min,
+			 double range_y_max){
+	
+	if(range_y_max - range_y_min <= 0 || range_x_max - range_x_min <= 0 ) return;
+	double x_scale = get_scale(g -> pixel_width, range_x_max, range_x_min);
+	double y_scale = get_scale(g -> pixel_height, range_y_max, range_y_min);
+	char **pix = g -> pix;
+	double y;
+	double Y_old;
+	int start = 0;
+	int i, j;
+	for(i = range_x_min*x_scale; i < range_x_max*x_scale; i++){
+		double x = (double)i/x_scale;
+
+		y = (*f)(x);		
+
+		int Y = (int) (y*y_scale) - range_y_min*y_scale;
+		if(start){
+			int h, l;
+			h = MAX(Y, Y_old);
+			l = MIN(Y, Y_old);
+			if(h - l < g -> pixel_height / 2)
+				for(j = l; j <= h; j++)
+					if(j > 0 && j < g ->pixel_height) 
+						pix[j][i - (int)(range_x_min*x_scale)] = (char)0x00;
+		}else start++;
+		Y_old = Y;
+	}
+}
+void draw_grapheft_2(graph *g,
+			 double (*f)(double),
+			 double range_x_min,
+			 double range_x_max,
+			 double range_y_min,
+			 double range_y_max,
+			 double range_X_min,
+			 double range_X_max,
+			 double range_Y_min,
+			 double range_Y_max){
+	
+	if(range_y_max - range_y_min <= 0 || range_x_max - range_x_min <= 0 ) return;
+	double X_scale = get_scale(g -> pixel_width, range_X_max, range_X_min);
+	double Y_scale = get_scale(g -> pixel_height, range_Y_max, range_Y_min);
+	char **pix = g -> pix;
+	double y;
+	double Y_old;
+	int start = 0;
+	int i, j;
+	for(i = range_X_min*X_scale; i < range_X_max*X_scale; i++){
+		
+		double x = (double)i/X_scale;
+
+		y = (*f)(x);		
+
+		int Y = (int) (y*Y_scale) - range_Y_min*Y_scale;
+		if(i > range_x_min*X_scale && i < range_x_max*X_scale){
+			if(start){
+				int h, l;
+				h = MAX(Y, Y_old);
+				l = MIN(Y, Y_old);
+				if(h - l < g -> pixel_height / 2)
+					for(j = l; j <= h; j++)
+						if(j > range_y_min*Y_scale - range_Y_min*Y_scale 
+						&& j < range_y_max*Y_scale - range_Y_min*Y_scale){ 
+							pix[j][i - (int)(range_X_min*X_scale)] = (char)0x00;
+						}
+			}else start++;
+		}
+		Y_old = Y;
+	}
+}
+void draw_grapheft_in_x(graph *g,
+			 double (*f)(double),
+			 double range_x_min,
+			 double range_x_max,
+			 double range_y_min,
+			 double range_y_max,
+			 double range_X_min,
+			 double range_X_max,
+			 double range_Y_min,
+			 double range_Y_max){
+	
+	if(range_y_max - range_y_min <= 0 || range_x_max - range_x_min <= 0 ) return;
+	double X_scale = get_scale(g -> pixel_width, range_X_max, range_X_min);
+	double Y_scale = get_scale(g -> pixel_height, range_Y_max, range_Y_min);
+	char **pix = g -> pix;
+	double y;
+	double Y_old;
+	int start = 0;
+	int i, j;
+	for(i = range_X_min*X_scale; i < range_X_max*X_scale; i++){
+		double x = (double)i/X_scale;
+
+		y = (*f)(x);		
+
+		int Y = (int) (y*Y_scale) - range_Y_min*Y_scale;
+		if(i < range_x_min*X_scale || i > range_x_max*X_scale){
+			if(start){
+				int h, l;
+				h = MAX_Y(Y, Y_old);
+				l = MIN_Y(Y, Y_old);
+				if(h - l < g -> pixel_height / 2)
+					for(j = l; j <= h; j++)
+						if(j > range_y_min*Y_scale - range_Y_min*Y_scale 
+						&& j < range_y_max*Y_scale - range_Y_min*Y_scale){ 
+							pix[j][i - (int)(range_X_min*X_scale)] = (char)0x00;
+						}
+			}else start++;
+		}
+		Y_old = Y;
+	}
+}
+graph *init(int x, int y){
+	graph *g = (graph *)malloc(sizeof(graph));
+	g -> pixel_width = x / 2;
+	g -> pixel_height = y / 2;
+
+	char **pix = (char **)malloc(sizeof(char *) * y/2);
+	int i = 0;
+	for(i = 0; i < y/2; i++) pix[i] = (char *)malloc(sizeof(char) *  x/2);
+
+	g -> pix = pix;
+	g -> h = fill_header(g -> pixel_width, g -> pixel_height);
+
+	return g;
+}
